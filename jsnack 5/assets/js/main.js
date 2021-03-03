74dea5455adf173097aa512944b8de9dfe030daf
Federico <federicofigini90@gmail.com>
2021-03-03 16:30:06 +0100

    finito

--- jsnack 5/assets/js/main.js
+++ jsnack 5/assets/js/main.js
@@ -4,7 +4,10 @@ var array = [];
 
 for (let i = 0; i < 6; i++) {
     var inserisciNumero = parseFloat(prompt("inserisci un numero"))
-    array.push(inserisciNumero)
+
+    if (inserisciNumero % 2 == 1) {
+        array.push(inserisciNumero)
+    }
 };
 
 output.innerHTML = array
